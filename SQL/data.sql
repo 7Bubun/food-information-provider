fe8b32e6a2484ccb0705a3656d69350dc6380ce3
7Bubun <j.grenda39@gmail.com>
2021-11-18 18:58:34 +0100

    Create SQL script template for inserting products information to the database

---
+++ SQL/data.sql
@@ -0,0 +1,118 @@
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'water',
+    'hydrates organism,enhances metabolism,provides minerals',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'apple',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'banana',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'orangelike',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'chocolate',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'egg',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'soft_drink',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'chips',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'bread',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'butter',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'milk',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'lemon',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'cheese',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'meat',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'tomato',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'cucumber',
+    '',
+    '',
+    ''
+);
+
+INSERT INTO PRODUCT_DATA(PRODUCT_NAME, PROS, CONS, ADDITIONAL_INFO) VALUES(
+    'ketchup',
+    '',
+    '',
+    ''
+);
