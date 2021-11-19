9a5788b06854317edc62d46c0062908a34f97d29
7Bubun <j.grenda39@gmail.com>
2021-11-19 13:59:53 +0100

    Add more information to DB

--- SQL/data.sql
+++ SQL/data.sql
@@ -1,135 +1,135 @@
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'water',
     '0',
-    'hydrates organism,enhances metabolism,provides minerals',
-    '',
-    ''
+    'hydrates organism,enhances metabolism,provides minerals,regulates temperature of body,detoxicates organism',
+    NULL,
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'apple',
     '47-52',
-    '',
-    '',
-    ''
+    'provides fiber,provides a lot of vitamins,prevents from cardiovascular diseases',
+    'some apples can have high level of pesticides,eating much apples may cause bloating',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'banana',
     '89',
-    '',
-    '',
-    ''
+    'provides potassium and magnesium, provides a lot of vitamins',
+    'ripe bananas are not recommended for diabetes',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'orangelike',
-    '53',
-    '',
-    '',
-    ''
+    '47-53',
+    'provides vitamin C,provides B-type vitamins,provides vitamin A, provides fiber',
+    NULL,
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'chocolate',
     '531-578',
-    '',
-    '',
-    ''
+    'provides magnessium,provides iron,provides niacin,boosts well-being',
+    'contains a lot of sugar,can cause constipations,can be addictive',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'egg',
     '155',
-    '',
-    '',
-    ''
+    'provides good quality protein,provides a lot of vitamins,provides minerals',
+    'may acidificate organism,contains a lot of cholesterol',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'soft_drink',
     '29-43',
-    '',
-    '',
-    ''
+    'provides energy',
+    'has extremely high level of sugar or chemical sweeteners,boosts aging,boosts cardiovascular diseases development',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'chips',
     '440-540',
-    '',
-    '',
-    ''
+    'provide a lot of energy',
+    'increase risk of atherosclerosis,increase level of cholesterol',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'bread',
     '265-313',
-    '',
-    '',
-    ''
+    'provides vitamins B1 and B2,provides vitamin PP,provides megnessium,provides zinc',
+    'bright-colored bread has high glycemic index',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'butter',
-    '',
-    '',
-    '',
-    ''
+    '740',
+    'contains linolenic acid,provides vitamin D,provides vitamin A,provides vitamin K',
+    'has high level of choresterol',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'milk',
-    '',
-    '',
-    '',
-    ''
+    '42-61',
+    'provides a lot of vitamins,provides minerals such as magnessium and potassium,provides some protein',
+    'contains saturated fat that increase risk of cardiovascular diseases occurrence',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'lemon',
-    '',
-    '',
-    '',
-    ''
+    '29',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'cheese',
-    '',
-    '',
-    '',
-    ''
+    '350-400',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'meat',
-    '',
-    '',
-    '',
-    ''
+    '242-272',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'tomato',
-    '',
-    '',
-    '',
-    ''
+    '20',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'cucumber',
-    '',
-    '',
-    '',
-    ''
+    '16',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET'
 );
 
 INSERT INTO PRODUCT_DATA(PRODUCT_NAME, ENERGY_VALUE_RANGE, PROS, CONS, ADDITIONAL_INFO) VALUES(
     'ketchup',
-    '',
-    '',
-    '',
-    ''
+    '112',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET',
+    'NOT IMPLEMENTED YET'
 );
